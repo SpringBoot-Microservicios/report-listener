233a6df35be634f833cb3dbfeaa5bca2ae3f657e
orlandorocahuapaya <132851982+orlandorocahuapaya@users.noreply.github.com>
2024-07-09 15:42:20 -0500

    Add files via upload

---
+++ report-listener/src/main/java/com/debuggeandoideas/reportlistener/documents/ReportDocument.java
@@ -0,0 +1,21 @@
+package com.debuggeandoideas.reportlistener.documents;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportDocument {
+
+    @Id
+    private String id;
+    private String content;
+
+}
